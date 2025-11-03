ed7e2e88027699ea2314ee576f204f0ee0cd59ac
L-htzl <146937456+L-htzl@users.noreply.github.com>
2025-11-03 12:00:08 +0100

    Update courbes.py

--- courbes.py
+++ courbes.py
@@ -1,4 +1,6 @@
 from random import * 
+import matplotlib.pyplot as plt
+import time
 
 def cfr_a_nbr (a,b,c):
     return a*100+b*10+c
@@ -23,10 +25,23 @@ def retirer (l,e):
         i+=1
     l= l[:i-1]+l[i:]
 
+def temps_naif(sudokunaif):
+    t0=time.time()
+    solution(sudokunaif)
+    t1=time.time()
+    return t1-t0
 
+def temps_sat(sudokusat):
+    t0=time.time()
+    sudoku(sudokunaif)
+    t1=time.time()
+    return t1-t0
+    
 
-
-def complexite (nbinit,nbfinal):
+def complexite (nbfinal):
+    x=[i for i in range(0,100)]
+    y_naif=[]
+    y_sat=[]
     sudokunaif= [[8, 9, 1, 2, 3, 4, 6, 7, 5],
      [3, 2, 5, 6, 8, 7, 4, 1, 9],
      [4, 7, 6, 9, 1, 5, 3, 2, 8],
@@ -36,13 +51,25 @@ def complexite (nbinit,nbfinal):
      [1, 4, 8, 5, 2, 6, 9, 3, 7],
      [7, 3, 2, 4, 9, 1, 5, 8, 6],
      [5, 6, 9, 8, 7, 3, 2, 4, 1]]
-    
     sudokusat=[[118],[129],[131],[142],[153],[164],[176],[187],[195],[213],[222],[235],[246],[258],[267],[274],[281],[299],[314],[327],[336],[349],[351],[365],[373],[382],[398],[419],[421],[434],[447],[455],[462],[478],[486],[493],[512],[525],[537],[543],[556],[568],[571],[589],[594],[616],[628],[633],[641],[654],[669],[677],[685],[692],[711],[724],[738],[745],[752],[766],[779],[783],[797],[817],[823],[832],[844],[859],[861],[875],[888],[896],[915],[926],[939],[948],[957],[963],[972],[984],[991]]
-    while len(sudokusat)> nbinit:
+    y_naif.append(temps_naif(sudokunaif))
+    y_sat.append(temps_sat(sudokusat))
+    
+    while len(sudokusat)> nbfinal:
         aleatoire = random.randint(1,999)
         (a,b,c) = nbr_a_cfr(aleatoire)
         sudokunaif[a][b]=0
+        retirer(sudokusat,aleatoire)
+        y_naif.append(temps_naif(sudokunaif))
+        y_sat.append(temps_sat(sudokusat))
+    plt.plot(x,y_naif,label='algo naïf')
+    plt.plot(x,y_sat,label='algo sat')
+    plt.title('complexité')
+    plt.xlabel("nombre d'inconnues")
+    plt.ylabel("temps")
+    plt.show()
         
+               
         
-        
+
         
