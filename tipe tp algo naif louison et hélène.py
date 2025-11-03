6a9c0f31fe8dfada0d336ef1bbca8b23ba851d9d
L-htzl <146937456+L-htzl@users.noreply.github.com>
2025-11-03 12:02:42 +0100

    Update tipe tp algo naif louison et hélène.py

--- tipe tp algo naif louison et hélène.py
+++ tipe tp algo naif louison et hélène.py
@@ -1,6 +1,16 @@
 #https://fr.doczz.net/doc/3595989/tp---algorithmes-de-backtracking-et-r%C3%A9solution-de-sudoku
 L=[[0, 9, 0, 2, 0, 0, 6, 0, 5], [3, 2, 0,0, 0, 7, 0, 0, 0], [0, 7, 0, 9, 0, 5, 0, 0,8], [0, 1, 0, 0, 0, 0, 0, 0, 0], [0, 0, 7,0, 0, 0, 0, 9, 4], [6, 0, 0, 0, 0, 0, 0, 0,0], [0, 0, 8, 0, 0, 0, 0, 0, 7], [0, 3, 0,4, 9, 1, 5, 0, 0], [0, 0, 0, 0, 0, 3, 0, 0,0]]
 
+def chiffrebloc(L,i,j):
+    l=[]
+    idepart=i-(i%3)
+    jdepart=j-(j%3)
+    for k in range(idepart,idepart+3):
+        for m in range(jdepart,jdepart+3):
+            if L[k][m] != 0:
+                l.append(L[k][m])
+    return l
+    
 def chiffreligne(L, i):
     l = []
     for j in L[i]:
@@ -15,35 +25,35 @@ def chiffrecolonnes(L,j):
             l.append(L[k][j])
     return l
 
-def chiffrebloc(L,i,j):
-    l=[]
-    departi=i-i%3
-    departj=j-j%3
-    for k in range (departi,departi +3):
-        for p in range (departj, departj +3):
-            if L[k][p]!=0:
-                l.append (L[k][p])
-    return l
-
 def chiffreconflit(L,i,j):
-    l1=chiffreligne(L,i)
-    l2=chiffrecolonnes(L,j)
-    l3=chiffrebloc(L,i,j)
-    return l1+l2+l3
+    impossible = chiffrebloc(L,i,j) + chiffreligne(L,i) + chiffrecolonnes(L,j)
+    return impossible
 
-def casesuivante (i,j):
-    if j ==8 : return i+1,0
-    return i,j+1
-        
 
+def casesuivante(i,j):
+    if j==8 : return i+1,0
+    return i,j+1 
+    
+    
 def solution(L):
     def aux (i,j):
         if i == 9 and j == 0:
             return True
         else:
+            (a,b)=casesuivante(i,j)
             if L[i][j]!=0:
-               aux(casesuivante(i,j))
+               return aux(a,b)
             l=[ k for k in range (1,10) if k not in chiffreconflit(L,i,j)]
-            for e in l:
-
+            if l==[] : return False
+            e=0
+            L[i][j]=l[e]
+            while not aux(a,b):
+                e+=1
+                if e>= len(l):
+                    L[i][j]=0
+                    return False 
+                L[i][j]=l[e]
+            return True
+    aux(0,0)
+    return L
 
